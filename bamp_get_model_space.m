6c8c575237b400d7be4f1fc20d8c628750006dae
Andreea Diaconescu <diaconescu@biomed.ee.ethz.ch>
2019-08-19 11:30:31 -0400

    Updated model space to included nonvola HGF models and RT models as a separate model space.

--- bamp_get_model_space.m
+++ bamp_get_model_space.m
@@ -1,16 +1,34 @@
 function [iCombPercResp]  = bamp_get_model_space
 
-iCombPercResp = zeros(15,2);
-iCombPercResp(1:3,1)   = 1;
-iCombPercResp(4:6,1)   = 2;
-iCombPercResp(7:9,1)   = 3;
-iCombPercResp(10:12,1) = 4;
-iCombPercResp(13:15,1) = 5;
+options = bamp_options();
+if options.model.RT == true
+    
+    iCombPercResp = zeros(4,2); % Only integrated models here
+    iCombPercResp(1,1)  = 1;
+    iCombPercResp(2,1)  = 2;
+    iCombPercResp(3,1)  = 3;
+    iCombPercResp(4,1)  = 4;
+    
+    iCombPercResp(1,2) = 1;
+    iCombPercResp(2,2) = 1;
+    iCombPercResp(3,2) = 1;
+    iCombPercResp(4,2) = 1;
+    
+else
+    iCombPercResp = zeros(18,2); % 6 x 3
+    iCombPercResp(1:3,1)   = 1;
+    iCombPercResp(4:6,1)   = 2;
+    iCombPercResp(7:9,1)   = 3;
+    iCombPercResp(10:12,1) = 4;
+    iCombPercResp(13:15,1) = 5;
+    iCombPercResp(16:18,1) = 6;
 
-iCombPercResp(1:3,2)   = 1:3;
-iCombPercResp(4:6,2)   = 4:6;
-iCombPercResp(7:9,2)   = 4:6;
-iCombPercResp(10:12,2) = 4:6;
-iCombPercResp(13:15,2) = 4:6;
+    iCombPercResp(1:3,2)   = 1:3;
+    iCombPercResp(4:6,2)   = 4:6;
+    iCombPercResp(7:9,2)   = 4:6;
+    iCombPercResp(10:12,2) = 4:6;
+    iCombPercResp(13:15,2) = 4:6;
+    iCombPercResp(16:18,2) = 4:6;
+end
 
 end
