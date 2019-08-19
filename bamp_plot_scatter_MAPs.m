35173ba00bf0b962d21348b40251ac8be592d9c2
Andreea Diaconescu <diaconescu@biomed.ee.ethz.ch>
2019-08-19 11:28:33 -0400

    Prettified plotting functions with these plots.

---
+++ bamp_plot_scatter_MAPs.m
@@ -0,0 +1,29 @@
+function bamp_plot_scatter_MAPs(selectedVariablePhases,Groups_Conditions,currentVAR,label)
+
+GroupingVariables = {'Controls','Psychopathy','Antisocial Personality Disorder'};
+
+figure;
+
+H   = selectedVariablePhases;
+N   = numel(selectedVariablePhases);
+
+switch currentVAR
+    case {'omega3'}
+        colors=bone(numel(H));
+    otherwise
+        colors=winter(numel(H));
+end
+for i=1:N
+    e = notBoxPlot(cell2mat(H(i)),cell2mat(Groups_Conditions(i)),'markMedian',true);
+    set(e.data,'MarkerSize', 10);
+    set(e.data,'Marker','o');
+    if i==1, hold on, end
+    set(e.data,'Color',colors(i,:));
+    set(e.sdPtch,'FaceColor',colors(i,:));
+    set(e.semPtch,'FaceColor',[0.9 0.9 0.9]);
+end
+set(gca,'XTick',1:N)
+set(gca,'XTickLabel',GroupingVariables);
+set(gca,'FontName','Calibri','FontSize',40);
+ylabel(label);
+end
