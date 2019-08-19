2269c514f454ac7bbd53f0b145d4b8d0c3e0178c
Andreea Diaconescu <diaconescu@biomed.ee.ethz.ch>
2019-08-19 11:29:23 -0400

    Updated stats function to print the stats table instead of opening a new figure.

--- bamp_stats_groups.m
+++ bamp_stats_groups.m
@@ -1,6 +1,14 @@
-function bamp_stats_groups(current_var,Groups)
+function bamp_stats_groups(current_var,varName,Groups)
 
-p = anovan(current_var,Groups,...
-    'model','interaction','varnames',{'Group'});
+[p, tbl, stats, terms] = anovan(current_var,Groups,...
+    'model','interaction','varnames',{'Group'},'display','off');
+
+ANOVA_CurrentVar = tbl;
+
+
+ANOVATbl =    cell2table(ANOVA_CurrentVar(2:end,:),...
+                        'VariableNames',{varName,'SumSq1',...
+                        'df','Singular1','MeanSq1','F1','PValue1'});
+disp(ANOVATbl);
 
 end
