c7801be5abca9efa23506c5b96609127becf15d5
Andreea Diaconescu <diaconescu@biomed.ee.ethz.ch>
2019-08-28 14:58:54 -0400

    Analysing both datatypes as default.

--- bamp_main.m
+++ bamp_main.m
@@ -4,6 +4,16 @@ function bamp_main
 % Important: you need to have access to the raw data; the path should be specified
 % in bamp_options();
 
+%% Analysis part 1
+options = bamp_options('Choice');
+fprintf('\n===\n\t Running the first level analysis (modelling trialwise choices:\n\n');
+
+loop_analyze_behaviour_local(options);
+
+fprintf('\n===\n\t Running group-level analysis and printing tables:\n\n');
+bamp_second_level(options);
+
+%% Analysis part 2
 options = bamp_options('RT_and_Choice');
 fprintf('\n===\n\t Running the first level analysis (modelling trialwise choices and RTs:\n\n');
 
