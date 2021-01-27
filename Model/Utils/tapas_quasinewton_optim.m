570b1a61902c770e8c92ba58065aa1952c03b32a
Andreea Diaconescu <diaconescu@biomed.ee.ethz.ch>
2021-01-26 19:04:46 -0500

    Updated functions so that the models can be run on more recent MATLAB versions.

--- Model/Utils/tapas_quasinewton_optim.m
+++ Model/Utils/tapas_quasinewton_optim.m
@@ -21,7 +21,7 @@ function optim = tapas_quasinewton_optim(f, init, varargin)
 %                  byproduct of optimization
 %
 % --------------------------------------------------------------------------------------------------
-% Copyright (C) 2012-2013 Christoph Mathys, TNU, UZH & ETHZ
+% Copyright (C) 2012-2019 Christoph Mathys, TNU, UZH & ETHZ
 %
 % This file is part of the HGF toolbox, which is released under the terms of the GNU General Public
 % Licence (GPL), version 3. You can redistribute it and/or modify it under the terms of the GPL
@@ -128,7 +128,13 @@ for i = 1:maxIter
         t       = 0.5^j;
         newx    = x+t.*descvec;
         newval  = f(newx);
-        dval    = newval-val;
+
+        % Regularize if the objective function value is Inf
+        if isinf(newval)
+            continue
+        else
+            dval = newval-val;
+        end
         
         % Stop if the new value is sufficiently smaller
         if dval < 1e-4*t*slope
@@ -164,7 +170,7 @@ for i = 1:maxIter
         continue
     else
         disp(' ')
-        disp('Warning: optimization terminated because the maximum number of resets is reached.')
+        disp('Warning: optimization terminated because the maximum number of resets was reached.')
         break
     end
     
@@ -219,7 +225,7 @@ for i = 1:maxIter
     % Warn if termination is only due to maximum of iterations being reached
     if i == maxIter
         disp(' ')
-        disp('Warning: optimization terminated because the maximum number of iterations is reached.')
+        disp('Warning: optimization terminated because the maximum number of iterations was reached.')
     end
 end
 
